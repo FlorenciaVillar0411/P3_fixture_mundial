ea90d17d818c2d6955e667f830a19e7237814cfd
Sabrina Taramasco <sabrinataramasco@hotmail.com>
2022-10-10 21:27:15 -0300

    PaisesController, Paises Vistas, falta  arreglar Buscar por codigo y id

--- WebMVC/Controllers/PaisesController.cs
+++ WebMVC/Controllers/PaisesController.cs
@@ -16,14 +16,27 @@ namespace WebMVC.Controllers
         public IAltaPais CUAltaPais { get; set; }
         public IListadoPaises CUListadoPaises { get; set; }
         public IListadoRegiones CUListadoRegiones { get; set; }
+        public IBajaPais CUBajaPais { get; set; }
+        public IModificarPais CUModificarPais { get; set; }
+        public IBuscarPais CUBuscarPais { get; set; }
 
 
-        public PaisesController(IListadoPaises cuPaises, IAltaPais cuAlta, IListadoRegiones cuRegiones)
+        public PaisesController(
+            IListadoPaises cuPaises,
+            IAltaPais cuAlta,
+            IListadoRegiones cuRegiones,
+            IBajaPais cuBajaPais,
+            IModificarPais cuModificarPais,
+            IBuscarPais cuBuscarPais)
         {
             CUAltaPais = cuAlta;
             CUListadoPaises = cuPaises;
             CUListadoRegiones = cuRegiones;
+            CUBajaPais = cuBajaPais;
+            CUModificarPais = cuModificarPais;
+            CUBuscarPais = cuBuscarPais;
         }
+
 
         // GET: Paises
         public ActionResult Index()
@@ -57,7 +70,8 @@ namespace WebMVC.Controllers
                 CUAltaPais.Alta(vm.Nuevo);
                 return RedirectToAction(nameof(Index));
             }
-            catch {
+            catch
+            {
 
                 ViewBag.Error = "error";
                 return View();
@@ -73,12 +87,11 @@ namespace WebMVC.Controllers
         // POST: Paises/Edit/5
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Edit(int id, IFormCollection collection)
+        public ActionResult Edit(int id, Pais pais)
         {
             try
             {
-                // TODO: Add update logic here
-
+                CUModificarPais.Modificar(pais);
                 return RedirectToAction(nameof(Index));
             }
             catch
@@ -100,9 +113,60 @@ namespace WebMVC.Controllers
         {
             try
             {
-                // TODO: Add delete logic here
-
+                CUBajaPais.Baja(id);
                 return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Paises/BuscarPorId
+        public ActionResult BuscarPorId(int id)
+        {
+            return View();
+        }
+
+        // POST: Paises/BuscarPorId
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult BuscarPorId(int id, IFormCollection collection)
+        {
+            try
+            {
+                Pais paisBuscado = CUBuscarPais.Buscar(id);
+                if (paisBuscado == null)
+                {
+                    ViewBag.msg = "No hay paises con ese id";
+                }
+                return View(paisBuscado);
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Paises/BuscarPorCodigo
+        public ActionResult BuscarPorCodigo(string codigo)
+        {
+            return View();
+        }
+
+        // POST: Paises/Buscar
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult BuscarPorCodigo(string codigo, IFormCollection collection)
+        {
+            try
+            {
+                Pais paisBuscado = CUBuscarPais.Buscar(codigo);
+                if (paisBuscado == null)
+                {
+                    ViewBag.msg = "No hay paises con ese codigo";
+                }
+                return RedirectToAction(nameof(paisBuscado));
             }
             catch
             {
